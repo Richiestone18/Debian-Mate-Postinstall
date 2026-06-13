49002db20270a240242ff44b77d82d1d124b21c5
Richiestone18 <kodipc18@proton.me>
2026-06-12 19:06:00 -0600

    Update debian-sid-mate-postinstall.sh

--- debian-sid-mate-postinstall.sh
+++ debian-sid-mate-postinstall.sh
@@ -124,7 +124,7 @@ apt install -y \
     firmware-ath9k-htc firmware-atheros \
     firmware-b43-installer firmware-b43legacy-installer \
     firmware-bnx2 firmware-bnx2x firmware-brcm80211 \
-    firmware-intel-sound  firmware-iwlwifi \
+    firmware-intel-sound firmware-iwlwifi \
     firmware-libertas firmware-misc-nonfree firmware-myricom firmware-netronome \
     firmware-netxen firmware-qcom-media firmware-qcom-soc firmware-qlogic \
     firmware-samsung firmware-siano \
